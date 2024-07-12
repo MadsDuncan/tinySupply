c708a1f1e40d770c64595a8d5b38dcda4d481360
Mads Larsen <madsduncan@gmail.com>
2024-07-13 00:55:57 +0200

    Added basic display app demo

--- main/display/display.h
+++ main/display/display.h
@@ -8,6 +8,7 @@
 #define DISPLAY_DEMO_HELLO_WORLD 2
 #define DISPLAY_DEMO_FULL_SCREEN 3
 #define DISPLAY_DEMO_THIN_RECT   4
+#define DISPLAY_DEMO_APP         5
 
 void display_setup();
 void display_start_demo(uint8_t demo);
